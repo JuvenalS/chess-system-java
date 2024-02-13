04a4d77764bd45f6681c605b8d38ce2efa981c6f
Juvenal Silveira da Silva Neto <silveiraneto@hotmail.com>
2024-02-13 11:39:23 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,22 @@
+package boardgame;
+
+public class Piece {
+
+	protected Position position;
+	private Board board;
+
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
+	@Override
+	public String toString() {
+		return "Piece [position=" + position + ", board=" + board + "]";
+	}
+
+}
